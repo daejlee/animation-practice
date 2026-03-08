4168246ecb9c07653a9d6cf12c11ce917f8fd60c
daejlee <savif4991@gmail.com>
2026-03-08 15:48:13 +0900

    feat: add freemode infinite card swiper

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import CardSwiper from "./components/CardSwiper";
+
+export default function Home() {
+  return (
+    <main>
+      <section>
+        <CardSwiper />
+      </section>
+    </main>
+  );
+}
